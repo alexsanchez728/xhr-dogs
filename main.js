a5f9739696a2ddc6706ee3226e71b01b0c4515f9
Alex Sanchez <alesanchez728@gmail.com>
2017-08-28 18:47:58 -0500

    reading dogs and breeds from JSON

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("here")
+function runThisWhenFileLoads() {
+	var data = JSON.parse(this.responseText).dogs;
+	console.log("dogs", data);
+}
+
+function runThisWhenFileLoads2() {
+	var data = JSON.parse(this.responseText).breeds;
+	console.log("breeds", data);
+}
+function didntWork() {
+	console.log("what did you do?!?");
+}
+
+
+var myRequestForDogs = new XMLHttpRequest();
+myRequestForDogs.addEventListener("load", runThisWhenFileLoads);
+myRequestForDogs.addEventListener("error", didntWork);
+myRequestForDogs.open("GET", "https://random-dogs-api.herokuapp.com/dogs/42");
+myRequestForDogs.send();
+
+
+var myRequestForDogBreeds = new XMLHttpRequest();
+myRequestForDogBreeds.addEventListener("load", runThisWhenFileLoads2);
+
+myRequestForDogBreeds.open("GET", "breeds.json");
+myRequestForDogBreeds.send();
+
+
+
+
+
+
