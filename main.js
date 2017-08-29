c1e730cc00e53d934e07c19f28c2a117f4a6d7fc
Alex Sanchez <alesanchez728@gmail.com>
2017-08-28 19:09:50 -0500

    added dogs api

--- main.js
+++ main.js
@@ -6,27 +6,21 @@ function runThisWhenFileLoads() {
 function runThisWhenFileLoads2() {
 	var data = JSON.parse(this.responseText).breeds;
 	console.log("breeds", data);
+
 }
+
 function didntWork() {
 	console.log("what did you do?!?");
 }
 
-
-var myRequestForDogs = new XMLHttpRequest();
+var myRequestForDogs = new XMLHttpRequest;
 myRequestForDogs.addEventListener("load", runThisWhenFileLoads);
 myRequestForDogs.addEventListener("error", didntWork);
 myRequestForDogs.open("GET", "https://random-dogs-api.herokuapp.com/dogs/42");
 myRequestForDogs.send();
 
-
-var myRequestForDogBreeds = new XMLHttpRequest();
+var myRequestForDogBreeds = new XMLHttpRequest;
 myRequestForDogBreeds.addEventListener("load", runThisWhenFileLoads2);
-
+myRequestForDogBreeds.addEventListener("error", didntWork);
 myRequestForDogBreeds.open("GET", "breeds.json");
 myRequestForDogBreeds.send();
-
-
-
-
-
-
